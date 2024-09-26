383d1f97fd0cb8ac7c0c2a4e66263f72b92ee24c
ImranParthib <imranparthib13@gmail.com>
2024-09-27 00:00:23 +0600

    Update xml and mian java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("net.objecthunter:exp4j:0.4.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/fxcalculator/MainActivity.java
+++ app/src/main/java/com/example/fxcalculator/MainActivity.java
@@ -11,9 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-Button digitButton;
-TextView primaryDisplay;
+    Button digitButton, operatorButton;
+    TextView primaryDisplay, secondaryDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,7 @@ TextView primaryDisplay;
             return insets;
         });
         primaryDisplay = findViewById(R.id.primaryTextView);
+        secondaryDisplay = findViewById(R.id.secondaryTextView);
     }
 
     public void digitFunction(View view) {
@@ -35,8 +42,41 @@ TextView primaryDisplay;
         } else {
             primaryDisplay.append(digit);
         }
-
     }
 
-}
+    public void clearFunction(View view) {
+        primaryDisplay.setText("0");
+        secondaryDisplay.setText("");
+    }
 
+    public void deleteFunction(View view) {
+        String currentText = primaryDisplay.getText().toString();
+        if (currentText.length() > 1) {
+            String newText = currentText.substring(0, currentText.length() - 1);
+            primaryDisplay.setText(newText);
+        } else {
+            primaryDisplay.setText("0");
+        }
+    }
+
+    public void operationFunction(View view) {
+        operatorButton = (Button) view;
+        String operator = operatorButton.getText().toString();
+        String currentText = primaryDisplay.getText().toString();
+        secondaryDisplay.setText(currentText + operator);
+        primaryDisplay.setText("0");
+    }
+
+    public void calculateFunction(View view) {
+        String expression = secondaryDisplay.getText().toString() + primaryDisplay.getText().toString();
+        try {
+            Expression exp = new ExpressionBuilder(expression).build();
+            double result = exp.evaluate();
+            DecimalFormat format = new DecimalFormat("0.##");
+            primaryDisplay.setText(format.format(result));
+            secondaryDisplay.setText(expression);
+        } catch (Exception e) {
+            primaryDisplay.setText("Error");
+        }
+    }
+}
